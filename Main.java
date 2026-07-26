0b74d404b87b7e5561eb73e25b00f18e3056be1b
Thes3us <171039607+Thes3us@users.noreply.github.com>
2026-07-26 16:57:53 +0530

    code cleaned

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -6,25 +6,29 @@ public class Main {
         int bal = 0;
         Scanner scanner = new Scanner(System.in);
         do {
-            System.out.println("Main Menu: \n1. Menu \n2. Check Balance. \n3. Topup Wallet \n4. Exit");
+            System.out.println("<=====Main Menu=====> \n1. Menu \n2. Check Balance \n3. Topup Wallet \n4. Exit\n<==================>");
+            System.out.print("Choose an option: ");
             n = scanner.nextLine();
-            
-            if(n.equals("1")){
-                System.out.println("This is the menu.");
+            System.out.println();
+            switch(n) {
+                case "1":
+                    System.out.println("This is the menu.");
+                    break;
+                case "2":
+                    System.out.println(bal + " is your balance.");
+                    break;
+                case "3":
+                    System.out.print("Enter the amount to Topup: ");
+                    bal = bal + scanner.nextInt();
+                    scanner.nextLine();
+                    System.out.println("Your new balance is: " + bal);
+                    break;
+                case "4":
+                    System.out.println("Exiting...");
+                    break;
+                default:
+                    System.out.println("Invalid option. Please try again.");
             }
-            
-            else if(n.equals("2")) {
-                System.out.println(bal + " is your balance.");
-            }
-
-            else if(n.equals("3")) {
-                System.out.println("Enter the amount to Topup.");
-                bal = bal + Integer.parseInt(scanner.nextLine());
-                System.out.println("Your new balance is: " + bal);
-            }
-
-            System.out.println("\n");
-
         } while (!n.equals("4"));
         scanner.close();
     }
